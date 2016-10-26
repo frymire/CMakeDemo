040910d2b1a18db41f2fec7062d1f44ba5492f75
Mark Frymire <mark.frymire@dac.us>
2016-10-26 17:26:43 -0400

    Trigger automated tests after each build

--- src/tests.cmake
+++ src/tests.cmake
@@ -1,6 +1,17 @@
 # Set up tests. 
 include(CTest)
  
+# Add a command to the build process that automatically runs tests whenever CMakeDemo is built.
+add_custom_command(
+	TARGET CMakeDemo
+	POST_BUILD 
+	COMMAND ctest
+	WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR} 
+	COMMENT "Automatically testing CMakeDemo for the new build..." 
+	VERBATIM
+)
+	
+
 # Does the application run?
 add_test(CMakeDemo-Runs CMakeDemo 25) 
 
@@ -9,12 +20,15 @@ add_test(CMakeDemo-Usage CMakeDemo)
 set_tests_properties(CMakeDemo-Usage PROPERTIES PASS_REGULAR_EXPRESSION "Usage:.*number")
 
 # Define a macro to simplify adding tests.
-macro(do_test name arg result)
+macro(doTest name arg result)
+
+	# Add the test and set its expected behavior.
 	add_test(CMakeDemo-${name} CMakeDemo ${arg})
 	set_tests_properties(CMakeDemo-${name} PROPERTIES PASS_REGULAR_EXPRESSION ${result})
-endmacro(do_test)
+	
+endmacro()
 
 # Use the macro to add 3 more tests.
-do_test(25 25 "25 is 5")
-do_test("Negative" -25 "-25 is 0")
-do_test("Small" 0.0001 "0.0001 is 0.01")
+doTest(25 25 "25 is 5")
+doTest("Negative" -25 "-25 is 0")
+doTest("Small" 0.0001 "0.0001 is 0.01")
